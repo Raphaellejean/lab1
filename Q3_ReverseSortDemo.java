ed5988b43149a32337b499f5389d11f672b33342
Raphaellejean <37092981+Raphaellejean@users.noreply.github.com>
2020-05-18 21:35:30 -0400

    Update Q3_ReverseSortDemo.java

--- Q3_ReverseSortDemo.java
+++ Q3_ReverseSortDemo.java
@@ -2,7 +2,7 @@ public class Q3_ReverseSortDemo {
 
   public static void main(String[] args){
     char[] unorderedLetters;
-    unorderedLetters = new char[]{'b', 'm', 'z', 'a', 'u'};
+    unorderedLetters = new char[]{'x', 'a', 'c', 'm', 'r', 'p'};
     reverseSort(unorderedLetters);
     for (int i = 0 ; i < unorderedLetters.length; i++ )
       System.out.print(unorderedLetters[i]);
@@ -12,6 +12,23 @@ public class Q3_ReverseSortDemo {
   public static void reverseSort(char[] values){
 
     //your code here
+    int i, j, Min; 
+    char tmp;
+
+      for (i = 0; i < values.length - 1; i++) 
+      {
+        Min = i;
+        for (j = i + 1; j < values.length; j++) 
+        {
+           if (values[j] > values[Min]) 
+           {
+             Min = j;
+           }
+       }
+        tmp = values[Min];
+        values[Min] = values[i];
+        values[i] = tmp;
+     }
   }
 
 }
